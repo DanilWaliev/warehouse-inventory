cf902222e31f9e8f770d638222cefda45be1f055
Danil Valiev <dnlvlv39@gmail.com>
2025-09-09 00:00:51 +0500

    refactor: split requests to API and pages into different files

--- cmd/web/api.go
+++ cmd/web/api.go
@@ -1,1 +1,15 @@
 package main
+
+import "net/http"
+
+func (app *application) createTMC(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) deleteTMC(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) editTMC(w http.ResponseWriter, r *http.Request) {
+
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,12 +4,17 @@ import "net/http"
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
+	// Запросы к страницам
 	mux.HandleFunc("/", app.root)
 	mux.HandleFunc("/signin", app.signIn)
 	mux.HandleFunc("/signup", app.signUp)
 	mux.HandleFunc("/signout", app.signOut)
 	mux.HandleFunc("/production", app.production)
-	//mux.HandleFunc("/production/tmc", app.createTMC)
+
+	// Запросы к API
+	mux.HandleFunc("/api/tmc/create", app.createTMC)
+	mux.HandleFunc("/api/tmc/delete", app.deleteTMC)
+	mux.HandleFunc("/api/tmc/edit", app.editTMC)
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
