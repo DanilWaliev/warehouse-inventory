4e5b5b407a93dc7561feaa506e4b8df37688a237
Danil Valiev <dnlvlv39@gmail.com>
2025-10-03 12:26:37 +0300

    removed logging jwt claims

--- pkg/handlers/auth/jwt.go
+++ pkg/handlers/auth/jwt.go
@@ -48,8 +48,6 @@ func (h *AuthHandler) CreateToken(role, fullname string) (string, error) {
 		"iat":      time.Now().Unix(),
 	}
 
-	fmt.Printf("%+v", claims)
-
 	// Создаем токен, подписываем и передаем строку в tokenString
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(key)
